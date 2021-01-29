8de4680b5b1ade6ddc94a41e04c784d6ed41fec7
AnthorNet <anthor.net@gmail.com>
2021-01-29 12:57:18 +0100

    Initial commit (Forked from SCPP)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# DSP-ProductionPlanner
-Dyson-Calculator Production Planner
+# Dyson-Calculator Production Planner aka "DSPCPP"
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+The production planner is able to produce factory chains for Dyson Sphere Program.
+A game from Youthcat Studio.
+
+[![DSPCPP](./img/readmeImage.jpg)](https://dyson-calculator.com/en/production-planner)
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/AnthorNet/DSP-ProductionPlanner/issues) for a list of proposed features (and known issues).
---
+++ src/DSPCPP.js
@@ -0,0 +1,542 @@
+/* global Intl, URL, gtag */
+
+import ProductionPlannerWorker from './Worker.js';
+
+export default class DSPCPP
+{
+    constructor()
+    {
+        // General stuffs
+        this.baseUrls                   = {};
+        this.doUpdateUrl                = true;
+
+        this.debug                      = false;
+        this.language                   = 'en';
+        this.translate                  = {};
+        this.productionContainer        = $('#productionContainer');
+
+        this.staticAssetsUrl            = "https://static.satisfactory-calculator.com";
+        this.gameDataUrl                = "https://satisfactory-calculator.com/" + this.language + "/api/game";
+
+        // Updater notice
+        this.scriptsVERSION             = Math.floor(Math.random() * Math.floor(999));
+        this.urlScriptsVERSION          = null;
+        this.intervalScriptsVERSION     = null;
+    }
+
+    start()
+    {
+        if(this.urlScriptsVERSION !== null)
+        {
+            this.intervalScriptsVERSION = setInterval(this.checkVersion.bind(this), 300 * 1000);
+        }
+
+        $.getJSON(this.gameDataUrl + '?v=' + this.scriptsVERSION, function(data)
+        {
+            this.buildingsData  = data.buildingsData;
+            this.itemsData      = data.itemsData;
+            this.toolsData      = data.toolsData;
+            this.recipesData    = data.recipesData;
+
+            for(let recipeId in this.recipesData)
+            {
+                if(this.recipesData[recipeId].className !== undefined && this.recipesData[recipeId].className.startsWith('/Game/FactoryGame/') === false)
+                {
+                    this.recipesData[recipeId].className = '/Game/FactoryGame/Recipes/' + this.recipesData[recipeId].className;
+                }
+            }
+
+            this.initiateGraph();
+        }.bind(this));
+    }
+
+    initiateGraph()
+    {
+        this.graphLayout        = null;
+        this.graph              = cytoscape({
+            container               : document.getElementById('productionNetwork'),
+            wheelSensitivity        : 0.05,
+            layout                  : undefined,
+            elements                : {nodes: {}, edges: {}},
+            style                   : [
+                {
+                    selector                : 'node',
+                    style                   : {
+                        padding                 : '64px',
+                        width                   : '384px',
+                        height                  : '384px'
+                    }
+                },
+                {
+                    selector                : 'node[nodeType="merger"], node[nodeType="splitter"]',
+                    style                   : {
+                        padding                 : '32px',
+                        width                   : '128px',
+                        height                  : '128px'
+                    }
+                },
+                {
+                    selector                : 'node[image]',
+                    style                   : {
+                        shape                   : 'square',
+                        'background-fit'        : 'contain',
+                        'background-image'      : 'data(image)',
+                        'background-opacity'    : 0
+                    }
+                },
+                {
+                    selector                : 'node[performance]',
+                    style                   : {
+                        shape                   : 'ellipse',
+                        'background-fit'        : 'none',
+                        'background-width'      : '75%',
+                        'background-height'     : '75%',
+                        'border-width'          : 'data(borderWidth)'
+                    }
+                },
+                {
+                    selector                : 'node[performanceColor]',
+                    style                   : {
+                        'border-color'          : 'data(performanceColor)'
+                    }
+                },
+                {
+                    selector                : 'node[label]',
+                    style                   : {
+                        'text-margin-y'         : '20px',
+                        label                   : 'data(label)',
+                        'text-valign'           : 'bottom',
+                        'font-size'             : '64px;',
+                        'text-wrap'             : 'wrap',
+                        color                   : '#FFFFFF'
+                    }
+                },
+
+                {
+                    selector                : 'edge',
+                    style                   : {
+                        'curve-style'           : 'bezier',
+                        'target-arrow-shape'    : 'triangle',
+                        'line-color'            : '#2b7ce9',
+                        'target-arrow-color'    : '#2b7ce9',
+                        opacity                 : 1,
+                        width                   : '15px'
+                    }
+                },
+                {
+                    selector                : 'edge[color]',
+                    style                   : {
+                        'line-color'            : 'data(color)',
+                        'target-arrow-color'    : 'data(color)'
+                    }
+                },
+                {
+                    selector                : 'edge[label]',
+                    style                   : {
+                        'text-margin-y'         : '-30px',
+                        'text-rotation'         : 'autorotate',
+                        label                   : 'data(label)',
+                        'font-size'             : '36px;',
+                        color                   : '#FFFFFF'
+                    }
+                }
+            ]
+        });
+
+
+        return this.setupEvents();
+    }
+
+    setupEvents()
+    {
+        $('.addOneItem').on('click', function(e){
+            let currentId       = $(e.currentTarget).attr('data-id'),
+                currentInput    = $('input.requireInput[data-id=' + currentId + ']');
+                e.preventDefault();
+
+            $(e.currentTarget).addClass('d-none').removeClass('d-flex');
+            currentInput.closest('.media').find('.stepBackward').removeClass('disabled');
+            currentInput.val(1).closest('.media').addClass('d-flex').removeClass('d-none');
+            $('#chooseItem').modal('hide');
+
+            this.triggerUpdateDebounce();
+        }.bind(this));
+
+        $('.stepUpdate').on('click', function(e){
+            e.preventDefault();
+
+            let inputGroup      = $(e.currentTarget).parent().parent();
+            let input           = inputGroup.find('input');
+            let dataValue       = parseInt($(e.currentTarget).attr('data-value'));
+            let currentValue    = parseInt(input.val());
+            //let maxQty          = parseInt(input.attr('data-maxQty'));
+            let newValue        = currentValue + dataValue;
+
+            if(newValue >= 0/* && newValue <= maxQty*/)
+            {
+                if(newValue === 0)
+                {
+                    inputGroup.closest('.media').addClass('d-none').removeClass('d-flex');
+                    $('.addOneItem[data-id=' + input.attr('data-id') + ']').addClass('d-flex').removeClass('d-none');
+                }
+
+                input.val(newValue);
+                this.triggerUpdateDebounce();
+            }
+
+            inputGroup.find('.stepUpdate').removeClass('disabled');
+            if((newValue - 10) < 0){ inputGroup.find('.fastBackward').addClass('disabled'); }
+            if((newValue - 1) < 0){ inputGroup.find('.stepBackward').addClass('disabled'); }
+            //if((newValue + 1) > maxQty){ inputGroup.find('.stepForward').addClass('disabled'); }
+            //if((newValue + 10) > maxQty){ inputGroup.find('.fastForward').addClass('disabled'); }
+        }.bind(this));
+
+        $('input.requireInput').on('keyup', function(e){
+            /*
+            if($(e.currentTarget).val() > parseInt($(e.currentTarget).attr('data-maxQty')))
+            {
+                $(e.currentTarget).val(parseInt($(e.currentTarget).attr('data-maxQty')));
+            }
+            */
+
+            if(parseInt($(e.currentTarget).val()) === 0)
+            {
+                $(e.currentTarget).closest('.media').addClass('d-none').removeClass('d-flex');
+                $('.addOneItem[data-id=' + $(e.currentTarget).attr('data-id') + ']').addClass('d-flex').removeClass('d-none');
+            }
+
+            this.triggerUpdateDebounce();
+        }.bind(this));
+
+        $('select.requireInput').on('change', function(e){
+            this.triggerUpdateDebounce();
+        }.bind(this));
+
+        // Switch available options based on view mode
+        $('select[name="view"]').on('change', function(){
+            let currentValue = $(this).val();
+                $('div[data-view][data-view!="' + currentValue + '"]').hide();
+                $('div[data-view="' + currentValue + '"]').show();
+        });
+        $('select[name="mergeBuildings"]').on('change', function(){
+            let currentValue = $(this).val();
+                if(currentValue > 0)
+                {
+                    $('div[data-mergeBuildings]').show();
+                }
+                else
+                {
+                    $('div[data-mergeBuildings]').hide();
+                }
+        });
+
+        return this.updateRequired(true);
+    }
+
+    // Prevent too much worker starts...
+    triggerUpdateDebounce()
+    {
+        if(this.inputTimeout)
+        {
+            clearTimeout(this.inputTimeout);
+        }
+        this.inputTimeout = setTimeout(function(){
+            this.updateRequired();
+            this.inputTimeout = null;
+        }.bind(this), 500);
+    }
+
+    updateRequired(initialCall = false)
+    {
+        let  formData = {};
+
+        // Generate required list
+        $('.requireInput').each(function(){
+            if($(this).is('select'))
+            {
+                if(['mergeBuildings', 'useManifolds', 'minerOverclocking', 'pumpOverclocking', 'buildingOverclocking'].includes($(this).attr('name')))
+                {
+                    if(parseInt($(this).children("option:selected").val()) !== 1)
+                    {
+                        formData[$(this).attr('name')] = $(this).val();
+                    }
+                }
+                else
+                {
+                    if($(this).children("option:selected").val() !== '')
+                    {
+                        formData[$(this).attr('name').replace('[]', '')] = $(this).children("option:selected").val();
+                    }
+                }
+            }
+            else
+            {
+                // General items...
+                if($(this).val() > 0)
+                {
+                    if($(this).attr('data-id') !== undefined)
+                    {
+                        formData[$(this).attr('data-id')] = $(this).val();
+                    }
+                    else
+                    {
+                        formData[$(this).attr('name')] = $(this).val();
+                    }
+                }
+            }
+        }).promise().done(function(){
+            this.calculate(formData, initialCall);
+        }.bind(this));
+    }
+
+    reset()
+    {
+        // Reset Network Graph
+        this.graph.elements().remove();
+
+        // Terminate worker if needed
+        if(this.worker !== undefined && this.worker !== null)
+        {
+            this.worker.terminate();
+            this.worker = null;
+        }
+
+        $('#productionList').empty();
+        $('#itemsList').empty();
+        $('#buildingsList').empty();
+        $('#requiredPower').empty();
+
+        this.hideLoader();
+    }
+
+    calculate(formData, initialCall)
+    {
+        this.reset();
+
+        let $this       = this;
+        let blobScript  = new Blob([ '(', ProductionPlannerWorker.toString(), ')()' ], { type: 'application/javascript' });
+        let blobURL     = URL.createObjectURL(blobScript);
+            this.worker = new Worker(blobURL);
+
+            setTimeout(function(){ URL.revokeObjectURL(blobURL); }, 1500);
+
+        // Planner states
+        this.worker.onmessage = function(e)
+        {
+            if(e.data.type === 'showLoader')
+            {
+                $this.showLoader();
+                return;
+            }
+            if(e.data.type === 'updateLoaderText')
+            {
+                $this.updateLoaderText(e.data.text);
+                return;
+            }
+
+            if(e.data.type === 'updateUrl')
+            {
+                if(initialCall === false && $this.doUpdateUrl !== false)
+                {
+                    $this.updateUrl(e.data.url);
+                }
+
+                return;
+            }
+            if(e.data.type === 'updateGraphNetwork')
+            {
+                $this.updateGraphNetwork(e.data.nodes, e.data.edges, e.data.direction);
+                return;
+            }
+            if(e.data.type === 'updateRequiredPower')
+            {
+                $('#requiredPower').html(
+                    new Intl.NumberFormat($this.language)
+                            .format(Math.ceil(e.data.power)) + ' MW'
+                );
+                return;
+            }
+
+            if(e.data.type === 'updateTreeList')
+            {
+                $this.updateTreeList(e.data.html);
+                return;
+            }
+            if(e.data.type === 'updateItemsList')
+            {
+                $this.updateItemsList(e.data.html);
+                return;
+            }
+            if(e.data.type === 'updateBuildingsList')
+            {
+                $this.updateBuildingsList(e.data.html);
+                return;
+            }
+
+            if(e.data.type === 'done')
+            {
+                this.terminate();
+                return;
+            }
+
+            console.log('onmessage received:', e.data);
+        };
+
+        // Launch it!
+        this.worker.postMessage({
+            baseUrls    : this.baseUrls,
+            debug       : this.debug,
+            language    : this.language,
+            translate   : this.translate,
+
+            buildings   : this.buildingsData,
+            items       : this.itemsData,
+            tools       : this.toolsData,
+            recipes     : this.recipesData,
+
+            formData    : formData
+        });
+    }
+
+
+    updateGraphNetwork(dataNodes, dataEdges, direction = 'RIGHT')
+    {
+        if(this.graphLayout !== null)
+        {
+            this.graphLayout.stop();
+        }
+
+        this.graph.json({
+            elements: {
+                nodes: dataNodes,
+                edges: dataEdges
+            }
+        });
+
+        let layoutOptions = {
+                name                            : 'elk',
+                nodeDimensionsIncludeLabels     : true,
+                fit                             : true,
+                ranker                          : 'longest-path',
+                elk                             : {
+                    'elk.layered.spacing.nodeNodeBetweenLayers'     : 768,
+                    'elk.layered.spacing.nodeNode'                  : 512,
+                    'elk.direction'                                 : direction,
+                    'elk.algorithm'                                 : 'layered',
+                    'elk.layered.crossingMinimization.strategy'     : 'LAYER_SWEEP',
+                    'elk.edgeRouting'                               : "ORTHOGONAL",
+
+                    spacing                                         : 512,
+                    inLayerSpacingFactor                            : 50,
+                    layoutHierarchy                                 : true,
+                    intCoordinates                                  : true,
+                    zoomToFit                                       : true,
+                    separateConnectedComponents                     : false
+                }
+        };
+
+        this.graphLayout = this.graph.layout(layoutOptions);
+        this.graphLayout.on('layoutstop', function(){ this.hideLoader(); }.bind(this));
+
+        this.graphLayout.run();
+    }
+
+    updateTreeList(html)
+    {
+        $('#productionList').empty().html(html);
+
+        $('#productionList .collapseChildren')
+            .css('cursor', 'pointer')
+            .bind('click', function(){
+                let nextChild = $(this).parent().next('.parent');
+                    if(nextChild.is(":hidden"))
+                    {
+                        nextChild.show();
+                    }
+                    else
+                    {
+                        nextChild.hide();
+                    }
+            });
+    }
+
+    updateItemsList(html)
+    {
+        $('#itemsList').html(html);
+    }
+
+    updateBuildingsList(html)
+    {
+        $('#buildingsList').html(html);
+    }
+
+    hideLoader()
+    {
+        this.productionContainer.find('.loader').hide();
+    }
+    showLoader()
+    {
+        this.productionContainer.find('.loader').show();
+    }
+    updateLoaderText(text)
+    {
+        this.productionContainer.find('.loader h6').html(text);
+    }
+
+    updateUrl(url)
+    {
+        let urlJoined = this.baseUrls.planner + '/' + url.join('/');
+            window.history.pushState({href: urlJoined}, '', urlJoined);
+
+            if(typeof gtag === 'function')
+            {
+                gtag('event', 'page_view', {page_path: urlJoined});
+            }
+    }
+
+    checkVersion(currentVersion)
+    {
+        if(currentVersion !== undefined && currentVersion !== null)
+        {
+            if(currentVersion > this.scriptsVERSION)
+            {
+                if($.fn.modal)
+                {
+                    $('#newPlannerRelease').modal({backdrop: 'static', keyboard: false});
+                }
+                else
+                {
+                    alert("Good news, a new version of the production planner was released! Please refresh your page / browser cache to make sure you'll get the latest fixes and features.");
+                }
+
+                return false;
+            }
+        }
+        else
+        {
+            if(this.urlScriptsVERSION !== null)
+            {
+                $.get(this.urlScriptsVERSION, function(data){
+                    if(data > this.scriptsVERSION)
+                    {
+                        if($.fn.modal)
+                        {
+                            $('#newPlannerRelease').modal({backdrop: 'static', keyboard: false});
+                        }
+                        else
+                        {
+                            alert("Good news, a new version of the production planner was released! Please refresh your page / browser cache to make sure you'll get the latest fixes and features.");
+                        }
+
+                        clearInterval(this.intervalScriptsVERSION);
+                        return false;
+                    }
+                });
+            }
+        }
+
+        return true;
+    };
+}
+
+window.DSPCPP = new DSPCPP();
